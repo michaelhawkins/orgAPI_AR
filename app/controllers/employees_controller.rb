84d2b5602851fecaf2ae0c857cda86aedacaa09d
Michael Hawkins <mike@hawkinsunlimited.com>
2013-11-14 15:11:00 -0500

    Initial commit

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,19 @@
+class EmployeesController < ApplicationController
+  def new
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrgAPIAr::Application.config.secret_token = '0d5cefaeb049ad3b709f03250ec7c9fa6f1cb6e7472a6c8007a7055e86a1f2976ffac92692f38fa6d222f4c156c000039bb7813bd7ae95971c086adaa241743d'
